73e91dd83ea1c0cf66c6fb0ab0643a131bef385c
CallMeAL <alexpgarner@gmail.com>
2023-05-25 13:46:29 -0700

    started working on timer compoment

---
+++ frontend/src/components/Timer.jsx
@@ -0,0 +1,10 @@
+import Form from 'react-bootstrap/Form';
+const Timer = () => {
+  return (
+    <div className = 'timer'>
+        <span>00:00:00</span>
+    </div>
+  )
+}
+
+export default Timer
