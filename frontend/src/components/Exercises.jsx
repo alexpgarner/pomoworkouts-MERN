553463e4f6c63ea245cebe90105af2d629b27969
CallMeAL <alexpgarner@gmail.com>
2023-05-27 13:38:38 -0700

    added margins to .btn-timer-select container

--- frontend/src/components/Exercises.jsx
+++ frontend/src/components/Exercises.jsx
@@ -3,9 +3,9 @@ export const Exercises = ({exercise}) => {
     <div className='exercises'>
         <ul>
           <li>Name: {exercise.name} </li>
+					<li>Duration: {exercise.duration}min</li>
 					<li>type: {exercise.type}</li>
           <li>Target Muscle: {exercise.targetMuscleMain}</li>
-          <li>Duration: {exercise.duration}min</li>
           {exercise.description.map(element=><li key = {Math.floor(Math.random()*1000)}>{element}</li>)}
         </ul>
     </div>
